a0d2653fd85741a8f85a85cabad838bff7affcfd
Giovanni De Toni <giovanni.det@gmail.com>
2016-06-21 22:44:34 +0200

    Begin to add DAO implementations.

---
+++ src/main/java/it/unitn/disi/webprog2016/convictor/app/dao/interfaces/UserDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unitn.disi.webprog2016.convictor.app.dao.interfaces;
+
+import it.unitn.disi.webprog2016.convictor.app.beans.User;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Users DAO
+ * @author Giovanni De Toni
+ */
+public interface UserDAO {
+    
+    List<User> getAllUser();
+    User getUserById(int id);
+    void updateUser(User user);
+    void insertUser(User user);
+    
+} 
