7f3f55d0cdb92757f88c30ce8fe70335c8ac617f
Giovanni De Toni <giovanni.det@gmail.com>
2016-07-18 18:15:57 +0200

    Delete some comments.

--- src/main/java/it/unitn/disi/webprog2016/convictor/app/controllers/RestaurantsController.java
+++ src/main/java/it/unitn/disi/webprog2016/convictor/app/controllers/RestaurantsController.java
@@ -58,18 +58,6 @@ public class RestaurantsController extends AbstractController {
 		}
         
         // Set cusine filters (if present)
-        /*int [] cusineFilters = new int[allCusines.size()];
-        boolean thereAreFilters = false;
-        int i=0;
-        for (Cusine c : allCusines) {
-            if (request.getParameter(String.valueOf(c.getId())) != null) {
-                cusineFilters[i] = c.getId();
-                thereAreFilters = true;
-            } else {
-                cusineFilters[i] = -1;
-            }
-            i++;
-        }*/
         List<String> cusineFilters = new ArrayList<>();
         for (Cusine c: allCusines) {
             if (request.getParameter(String.valueOf(c.getId())) != null) {
